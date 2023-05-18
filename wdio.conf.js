177bbb9ca143086d4c81cfe0a7bd83bf73188289
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-05-18 12:46:25 +0200

    test: moved the chromedriver and updated the path

--- wdio.conf.js
+++ wdio.conf.js
@@ -103,7 +103,7 @@ exports.config = {
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
   services: [
-    ['chromedriver', { executablePath: 'chromdriver' }],
+    ['chromedriver', { executablePath: '/node_modules/bin/chromedriver' }],
   ],
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
