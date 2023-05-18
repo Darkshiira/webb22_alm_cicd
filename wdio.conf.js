82c429fe6e2eed230c7ecf47307abb4597111575
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-05-18 12:12:23 +0200

    test: baseurl changed

--- wdio.conf.js
+++ wdio.conf.js
@@ -86,7 +86,7 @@ exports.config = {
   // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
   // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
   // gets prepended directly.
-  baseUrl: 'http://webb22_alm_cicd:8080',
+  baseUrl: '8080:8080',
   //
   // Default timeout for all waitFor* commands.
   waitforTimeout: 10000,
